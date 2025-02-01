383380fc0b78011112b8f8e69bcde91fef6d49b6
abhinaypawar470 <abhinaypawar470@gmail.com>
2025-02-02 03:15:46 +0530

    my first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Define a route for the calculator
+@app.route("/", methods=["GET", "POST"])
+def calculator():
+    result = None
+    if request.method == "POST":
+        try:
+            data= request.json
+            main_data=data.get('data')
+            print("frontend Data: ", main_data)
+            result = eval(main_data)
+            response = {
+                "message": f"Data '{result}' received successfully!",
+                "status": "success",
+                "data": result  # Add the result to the response
+            }
+            return jsonify(response)
+        except (SyntaxError, ValueError, TypeError) as e:
+            return jsonify({"message": f"Error: {str(e)}", "status": "error"}), 400
+    return render_template("index.html")
+
+# Run the app
+if __name__ == "__main__":
+    app.run(debug=True)
