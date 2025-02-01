a10afd003ee740de9ac94a3394eb718cdb3ab2a2
abhinaypawar470 <abhinaypawar470@gmail.com>
2025-02-02 04:34:57 +0530

    Backend troubleshooting

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def calculator():
 # Run the app
 if __name__ == "__main__":
     port=int(os.environ.get("PORT",8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port,)
