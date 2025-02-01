0d599aef9de9dd98685b24f028af10931381f8db
abhinaypawar470 <abhinaypawar470@gmail.com>
2025-02-02 04:21:42 +0530

    port changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -25,4 +26,5 @@ def calculator():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT",8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
