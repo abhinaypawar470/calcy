42fdba4d6524dc9cecbf5dfe66681738be4d4fab
abhinaypawar470 <abhinaypawar470@gmail.com>
2025-02-02 21:45:15 +0530

    Changed fetch URL

--- app.py
+++ app.py
@@ -26,5 +26,6 @@ def calculator():
 
 # Run the app
 if __name__ == "__main__":
-    port=int(os.environ.get("PORT",8080))
-    app.run(host="0.0.0.0", port=port,)
+    # port=int(os.environ.get("PORT",8080))
+    # app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
